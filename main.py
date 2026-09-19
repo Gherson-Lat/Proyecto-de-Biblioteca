df527b0d3ea25b97d09270e024169a84f76f10bc
Santiago López <slopez@unimayor.edu.co>
2026-09-19 10:48:50 -0500

    Rutas catalogo

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from fastapi.responses import HTMLResponse
 # Importación de los routers de cada módulo
 # (Cada integrante exporta su APIRouter desde su carpeta)
 from loans.router_loans import router as router_loans
-# from catalog.router_catalog import router as router_catalog
+from catalog.catalog import router as router_catalog
 # from students.router_students import router as router_students
-#from penalty.router_penalty import router as router_penalty
+# from penalty.router_penalty import router as router_penalty
 # from reports.router_reports import router as router_reports
 
 app = FastAPI(
@@ -20,9 +20,9 @@ app = FastAPI(
 
 # Integración de los routers al tablero central
 app.include_router(router_loans)
-# app.include_router(router_catalog)
+app.include_router(router_catalog)
 # app.include_router(router_students)
-#app.include_router(router_penalty)
+# app.include_router(router_penalty)
 # app.include_router(router_reports)
 
 @app.get("/", response_class=HTMLResponse)
@@ -52,7 +52,7 @@ def dashboard_principal():
             </div>
             <div class="card">
                 <h3>Catálogo</h3>
-                <span>En construcción...</span>
+                <a href="/catalog/vista">Ir a Catálogo ➔</a>
             </div>
             <div class="card">
                 <h3>Estudiantes</h3>
